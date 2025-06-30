7d1e0c45cc4109c46dba2726ffcfaf495d674c8e
yunhanjo <gkswh0728@gmail.com>
2025-06-30 14:59:51 +0900

    last definition

--- bin/dart_rpg_game.dart
+++ bin/dart_rpg_game.dart
@@ -1,5 +1,7 @@
 import 'package:dart_rpg_game/dart_rpg_game.dart' as dart_rpg_game;
+import 'package:dart_rpg_game/test.dart';
+import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_rpg_game.calculate()}!');
+ //print('Hello world: ${dart_rpg_game.calculate()}!');
 }
--- test/dart_rpg_game_test.dart
+++ test/dart_rpg_game_test.dart
@@ -2,7 +2,5 @@ import 'package:dart_rpg_game/dart_rpg_game.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
+ 
 }
