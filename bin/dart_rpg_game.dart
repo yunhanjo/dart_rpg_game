2445af088ed238a4e87ecd13dd9e4c371b4ca686
t2024-m0214 <t2024-m0214@t2024-m0214-MacBook-Air.local>
2025-06-30 11:26:40 +0900

    first commit

---
+++ bin/dart_rpg_game.dart
@@ -0,0 +1,5 @@
+import 'package:dart_rpg_game/dart_rpg_game.dart' as dart_rpg_game;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_rpg_game.calculate()}!');
+}
---
+++ test/dart_rpg_game_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_rpg_game/dart_rpg_game.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
